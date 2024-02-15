3a6ea7eb0524a7b7dd6ee4fdca3d343c2495935b
yed5759 <yed5759@gmail.com>
2024-02-15 21:54:14 +0200

    commit web

--- src/Components/signup/Register.js
+++ src/Components/signup/Register.js
@@ -1,96 +1,114 @@
 import Valid from "./Valid";
 import inputs from "../../data/db.json";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
 import "./Style.css";
 
 function Register() {
-  const [inputsLists, setInputs] = useState(inputs);
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [imagePreview, setImagePreview] = useState(null);
+    const [inputsLists, setInputs] = useState(inputs);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [imagePreview, setImagePreview] = useState(null);
 
-  const handleImageChange = (event) => {
-    const file = event.target.files[0];
-    setSelectedImage(file);
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        setSelectedImage(file);
 
-    // Create a thumbnail of the selected image with dimensions 176x176
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      const img = new Image();
-      img.src = reader.result;
+        // Create a thumbnail of the selected image with dimensions 176x176
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            const img = new Image();
+            img.src = reader.result;
 
-      img.onload = () => {
-        const canvas = document.createElement("canvas");
-        const ctx = canvas.getContext("2d");
-        canvas.width = 176;
-        canvas.height = 176;
-        ctx.drawImage(img, 0, 0, 176, 176);
+            img.onload = () => {
+                const canvas = document.createElement("canvas");
+                const ctx = canvas.getContext("2d");
+                canvas.width = 176;
+                canvas.height = 176;
+                ctx.drawImage(img, 0, 0, 176, 176);
 
-        setImagePreview(canvas.toDataURL());
-      };
+                setImagePreview(canvas.toDataURL());
+            };
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
     };
 
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const submit = () => {
-    const input = {
-      "username": document.getElementById("username").value,
-      "password": document.getElementById("password").value,
-      "password_again": document.getElementById("password_again").value,
-      "nickname": document.getElementById("nickname").value,
-      "imageType": selectedImage?.type || ""
+    const submit = () => {
+        const input = {
+            "username": document.getElementById("username").value,
+            "password": document.getElementById("password").value,
+            "password_again": document.getElementById("password_again").value,
+            "nickname": document.getElementById("nickname").value,
+            "imageType": selectedImage?.type || ""
+        };
+        setInputs([input,...inputsLists]);
     };
-    setInputs([input]);
-  };
 
-  return (
-      <div className='wrapper'>
-          <form>
-            <div className="input-box">
-            <input type="text" id="username" placeholder='Username'></input>
+    return (
+        <div className="mask">
+            <div className="wrapper">
+                <form onSubmit={submit} className="needs-validation" noValidate>
+                    <div className="mb-3">
+                        <label htmlFor="username"
+                               className="form-label">username</label>
+                        <input className="form-control" type="text"
+                               id="username"
+                               placeholder='Username' required/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password"
+                               className="form-label">password</label>
+                        <input className="form-control" type="password"
+                               id="password"
+                               placeholder='Password' required/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password"
+                               className="form-label">password twice</label>
+                        <input className="form-control" type="password"
+                               id="password_again"
+                               placeholder='Password again' required/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor='nickname' className="form-label">display
+                            name</label>
+                        <input className="form-control" type="text"
+                               id="nickname"
+                               placeholder='Nickname' required/>
+                    </div>
+                    <div className="d-flex justify-content-betweenmb-3">
+                        <label className="form-label">Profile Image:</label>
+                        <input className="form-control" type="file"
+                               id="formfile" accept="image/*"
+                               onChange={handleImageChange} required/>
+                        {imagePreview && (
+                            <img
+                                src={imagePreview}
+                                alt="Selected"
+                                style={{maxWidth: "100%", maxHeight: "100%"}}
+                            />
+                        )}
+                    </div>
+                    <div className="d-flex justify-content-between">
+                        <button type="submit" className="btn btn-primary w-25">
+                            Sign up
+                        </button>
+                        <p><a href="../Login"
+                              className="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
+                            <Link to='/'>Login</Link>
+                        </a></p>
+                    </div>
+                    <div>
+                        {inputsLists.map((input) => (
+                            <Valid {...input} />
+                        ))}
+                    </div>
+                </form>
             </div>
-            <div className="input-box">
-            <input type="password" id="password" placeholder='Password'></input>
-            </div>
-              <div className="input-box">
-            <input type="password" id="password_again" placeholder='Password again'></input>
-              </div>
-                <div className="input-box">
-            <input type="text" id="nickname" placeholder='Nickname'></input>
-                </div>
-            <label>Profile Image:</label>
-            <br></br>
-            <input
-                type="file"
-                id="profile"
-                accept="image/*"
-                onChange={handleImageChange}
-            ></input>
-            <br></br>
-            {imagePreview && (
-                <img
-                    src={imagePreview}
-                    alt="Selected"
-                    style={{ maxWidth: "100%", maxHeight: "100%" }}
-                />
-            )}
-            <br></br>
-            <button type="button" onClick={submit} className="button">
-              Sign up
-            </button>
-            <Link to='/' className="button" id="Login">Login</Link>
-            <br></br>
-            <div>
-              {inputsLists.map((input) => (
-                  <Valid {...input} />
-              ))}
-            </div>
-          </form>
-      </div>
-  );
+        </div>
+    );
 }
 
 export default Register;
