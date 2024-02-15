2d914ee13431125fdb5f9de164091af7afda59be
itamar3188 <itamarc946@gmail.com>
2024-02-15 19:20:28 +0200

    .

--- src/Components/feed/Feed.js
+++ src/Components/feed/Feed.js
@@ -17,6 +17,7 @@ function Feed() {
         if (!state) {
             navigate(-1);
         }
+
     }, [state, navigate]);
     const username = state ? state.username : null
     const addPost = (newPost) => {
